1f035b75b842a69d4be15c79f6256668c727e15d
maxgliantsev <143866680+whypxralich@users.noreply.github.com>
2024-02-24 00:04:45 +0300

    Create stepik_5.1_yes_or_not_that_is_the_question.py

---
+++ stepik_5.1_yes_or_not_that_is_the_question.py
@@ -0,0 +1,9 @@
+x = int(input()) 
+if x % 2 != 0:
+    print ("YES")
+if 2 <= x <= 5 and x % 2 == 0:
+    print ("NO")
+if 6 <= x <= 20 and x % 2 == 0:
+    print ("YES")
+if 20 < x and x % 2 == 0:
+    print ("NO")
